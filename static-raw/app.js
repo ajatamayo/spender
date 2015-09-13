9c4021ace71b00d0bd6590542b6cb3a250533968
ajatamayo <ajatamayo@gmail.com>
2015-09-13 14:11:40 +0800

    Add styles

--- static-raw/app.js
+++ static-raw/app.js
@@ -1,26 +1,58 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Prepare the cards in the stack for iteration.
-    cards = [].slice.call(document.querySelectorAll('.stack--card'))
+  // Get the stuffs
+  main_form = document.querySelector('#main-form');
 
-    // An instance of the Stack is used to attach event listeners.
-    stack = gajus.Swing.Stack();
+  // Prepare the cards in the stack for iteration.
+  cards = [].slice.call(document.querySelectorAll('.stack--card'))
 
-    cards.forEach(function (targetElement) {
-        // Add card element to the Stack.
-        stack.createCard(targetElement);
-    });
+  // An instance of the Stack is used to attach event listeners.
+  stack = gajus.Swing.Stack();
 
-    // Add event listener for when a card is thrown out of the stack.
-    stack.on('throwout', function (e) {
-        // e.target Reference to the element that has been thrown out of the stack.
-        // e.throwDirection Direction in which the element has been thrown (Card.DIRECTION_LEFT, Card.DIRECTION_RIGHT).
+  cards.forEach(function (targetElement) {
+    // Add card element to the Stack.
+    stack.createCard(targetElement);
+  });
 
-        console.log('Card has been thrown out of the stack.');
-        console.log('Throw direction: ' + (e.throwDirection == gajus.Swing.Card.DIRECTION_LEFT ? 'left' : 'right'));
-    });
+  // Add event listener for when a card is thrown out of the stack.
+  stack.on('throwout', function (e) {
+    // e.target Reference to the element that has been thrown out of the stack.
+    // e.throwDirection Direction in which the element has been thrown (Card.DIRECTION_LEFT, Card.DIRECTION_RIGHT).
 
-    // Add event listener for when a card is thrown in the stack, including the spring back into place effect.
-    stack.on('throwin', function (e) {
-        console.log('Card has snapped back to the stack.');
-    });
+    console.log('Card has been thrown out of the stack.');
+    console.log('Throw direction: ' + (e.throwDirection == gajus.Swing.Card.DIRECTION_LEFT ? 'left' : 'right'));
+  });
+
+  // Add event listener for when a card is thrown in the stack, including the spring back into place effect.
+  stack.on('throwin', function (e) {
+    console.log('Card has snapped back to the stack.');
+  });
+
+  // Add event listener for when the card is moved to determine what form should be shown below
+  stack.on('dragmove', function (e) {
+    var direction = e.throwDirection == gajus.Swing.Card.DIRECTION_RIGHT ? 'right' : 'left';
+
+    if (direction == 'right') {
+      // Like! Income!
+      removeClass(main_form, 'stack--form__expense');
+      addClass(main_form, 'stack--form__income');
+    } else {
+      // Dislike! Expense!
+      removeClass(main_form, 'stack--form__income');
+      addClass(main_form, 'stack--form__expense');
+    }
+  });
 });
+
+var addClass = function(el, className) {
+  if (el.classList)
+    el.classList.add(className);
+  else
+    el.className += ' ' + className;
+}
+
+var removeClass = function(el, className) {
+  if (el.classList)
+    el.classList.remove(className);
+  else
+    el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+}
