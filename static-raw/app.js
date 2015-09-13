cf2b5a0bc2984011391bd6af6e3e8af3bbe4d709
ajatamayo <ajatamayo@gmail.com>
2015-09-13 14:49:21 +0800

    Restructure javascript

--- static-raw/app.js
+++ static-raw/app.js
@@ -1,64 +1,100 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Get the stuffs
-  body = document.querySelector('body');
-  main_form = document.querySelector('#main-form');
+var SPENDER = (function(SPENDER) {
 
-  // Prepare the cards in the stack for iteration.
-  cards = [].slice.call(document.querySelectorAll('.stack--card'))
+  /**
+   * Utility function declarations
+   */
 
-  // An instance of the Stack is used to attach event listeners.
-  stack = gajus.Swing.Stack();
+  var addClass = function(el, className) {
+    if (el.classList)
+      el.classList.add(className);
+    else
+      el.className += ' ' + className;
+  }
 
-  cards.forEach(function (targetElement) {
-    // Add card element to the Stack.
-    stack.createCard(targetElement);
-  });
+  var removeClass = function(el, className) {
+    if (el.classList)
+      el.classList.remove(className);
+    else
+      el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+  }
 
-  // Add event listener for when a card is thrown out of the stack.
-  stack.on('throwout', function (e) {
+  /**
+   * Start of actual cool stuff
+   */
+
+  SPENDER.init = function() {
+    document.addEventListener('DOMContentLoaded', function () {
+      SPENDER.initializeControls();
+      SPENDER.initializeStack();
+    });
+  };
+
+  SPENDER.initializeControls = function() {
+    SPENDER.body = document.querySelector('body');
+    SPENDER.main_form = document.querySelector('#main-form');
+  };
+
+  SPENDER.initializeStack = function() {
+    // Prepare the cards in the stack for iteration.
+    cards = [].slice.call(document.querySelectorAll('.stack--card'))
+
+    // An instance of the Stack is used to attach event listeners.
+    stack = gajus.Swing.Stack();
+
+    cards.forEach(function (targetElement) {
+      // Add card element to the Stack.
+      stack.createCard(targetElement);
+    });
+
+    // Add event listener for when a card is thrown out of the stack.
+    stack.on('throwout', SPENDER.onThrowout);
+
+    // Add event listener for when a card is thrown in the stack, including the spring back into place effect.
+    stack.on('throwin', SPENDER.onThrowin);
+
+    // Add event listener for when the card is moved to determine what form should be shown below
+    stack.on('dragmove', SPENDER.onDragmove);
+  };
+
+  SPENDER.onThrowout = function (e) {
     // e.target Reference to the element that has been thrown out of the stack.
     // e.throwDirection Direction in which the element has been thrown (Card.DIRECTION_LEFT, Card.DIRECTION_RIGHT).
 
     console.log('Card has been thrown out of the stack.');
     console.log('Throw direction: ' + (e.throwDirection == gajus.Swing.Card.DIRECTION_LEFT ? 'left' : 'right'));
-  });
+  };
 
-  // Add event listener for when a card is thrown in the stack, including the spring back into place effect.
-  stack.on('throwin', function (e) {
-    removeClass(body, 'g-alert');
-    removeClass(body, 'g-okaygo');
-  });
+  SPENDER.onThrowin = function (e) {
+    removeClass(SPENDER.body, 'g-alert');
+    removeClass(SPENDER.body, 'g-okaygo');
+  };
 
-  // Add event listener for when the card is moved to determine what form should be shown below
-  stack.on('dragmove', function (e) {
+  SPENDER.onDragmove = function (e) {
     var direction = e.throwDirection == gajus.Swing.Card.DIRECTION_RIGHT ? 'right' : 'left';
 
     if (direction == 'right') {
-      // Like! Income!
-      removeClass(body, 'g-alert');
-      addClass(body, 'g-okaygo');
-      removeClass(body, 'stack--form__expense');
-      addClass(body, 'stack--form__income');
+      SPENDER.onSwiperight();
     } else {
-      // Dislike! Expense!
-      removeClass(body, 'g-okaygo');
-      addClass(body, 'g-alert');
-      removeClass(body, 'stack--form__income');
-      addClass(body, 'stack--form__expense');
+      SPENDER.onSwipeleft();
     }
-  });
-});
+  };
 
-var addClass = function(el, className) {
-  if (el.classList)
-    el.classList.add(className);
-  else
-    el.className += ' ' + className;
-}
+  SPENDER.onSwiperight = function() {
+    removeClass(SPENDER.body, 'g-alert');
+    addClass(SPENDER.body, 'g-okaygo');
+    removeClass(SPENDER.main_form, 'stack--form__expense');
+    addClass(SPENDER.main_form, 'stack--form__income');
+  };
 
-var removeClass = function(el, className) {
-  if (el.classList)
-    el.classList.remove(className);
-  else
-    el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-}
+  SPENDER.onSwipeleft = function() {
+    removeClass(SPENDER.body, 'g-okaygo');
+    addClass(SPENDER.body, 'g-alert');
+    removeClass(SPENDER.main_form, 'stack--form__income');
+    addClass(SPENDER.main_form, 'stack--form__expense');
+  };
+
+  return SPENDER;
+
+}(SPENDER || {}));
+
+SPENDER.init();
